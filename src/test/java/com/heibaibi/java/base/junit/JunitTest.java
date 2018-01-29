52a719325c21837be5b23929b05d3c31d73791aa
来来 <liulailaiok@163.com>
2018-01-29 13:11:58 +0800

    測試junit。

---
+++ src/test/java/com/heibaibi/java/base/junit/JunitTest.java
@@ -0,0 +1,16 @@
+package com.heibaibi.java.base.junit;
+
+import org.junit.Test;
+
+public class JunitTest {
+
+    @Test
+    public void function() {
+        System.out.println("测试junit工具");
+    }
+
+    @Test
+    public void function2() {
+        System.out.println("测试junit工具");
+    }
+}
